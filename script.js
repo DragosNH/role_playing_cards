8bee734be238fe733a3374ebc60b4306e387e13e
Dragoş <edragos33@gmail.com>
2025-09-18 17:12:17 +0200

    togle active btn

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 let warriorBtn = document.querySelector(".warrior-btn");
 let mageBtn = document.querySelector(".mage-btn");
 let thiefBtn = document.querySelector(".thief-btn");
+let conatiner = document.querySelectorAll(".container");
+let buttons = document.querySelectorAll(".btn");
+
+
+warriorBtn.addEventListener("click", () => {
+  if (warriorBtn.classList.contains("active")) return; 
+
+  warriorBtn.classList.add("active");
+
+  const card = document.createElement("div");
+  card.style.borderTop = "4px ridge var(--golden-yellow)";
+  card.style.borderBottom = "4px groove var(--golden-yellow)";
+  card.style.borderRight = "4px ridge var(--golden-yellow)";
+  card.style.borderLeft = "4px groove var(--golden-yellow)";
+  card.style.borderRadius = "8px";
+  card.style.margin = "15px";
+  card.style.width = "150px";
+  card.style.height = "150px";
+
+  document.body.appendChild(card);
+});
 
