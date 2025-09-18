2913f68bff520132f34c534f06ad72d175e271d0
Dragoş <edragos33@gmail.com>
2025-09-18 17:22:48 +0200

    add and remove card on click

--- script.js
+++ script.js
@@ -6,20 +6,39 @@ let buttons = document.querySelectorAll(".btn");
 
 
 warriorBtn.addEventListener("click", () => {
-  if (warriorBtn.classList.contains("active")) return; 
+    if (warriorBtn.classList.contains("active")) return; 
 
-  warriorBtn.classList.add("active");
+    warriorBtn.classList.add("active");
 
-  const card = document.createElement("div");
-  card.style.borderTop = "4px ridge var(--golden-yellow)";
-  card.style.borderBottom = "4px groove var(--golden-yellow)";
-  card.style.borderRight = "4px ridge var(--golden-yellow)";
-  card.style.borderLeft = "4px groove var(--golden-yellow)";
-  card.style.borderRadius = "8px";
-  card.style.margin = "15px";
-  card.style.width = "150px";
-  card.style.height = "150px";
+    const card = document.createElement("div");
+    card.style.borderTop = "4px ridge var(--golden-yellow)";
+    card.style.borderBottom = "4px groove var(--golden-yellow)";
+    card.style.borderRight = "4px ridge var(--golden-yellow)";
+    card.style.borderLeft = "4px groove var(--golden-yellow)";
+    card.style.borderRadius = "8px";
+    card.style.margin = "15px";
+    card.style.width = "150px";
+    card.style.height = "150px";
 
-  document.body.appendChild(card);
+    const closeBtn = document.createElement("button");
+    closeBtn.textContent ="X";
+    closeBtn.style.borderTop = "4px ridge var(--golden-yellow)";
+    closeBtn.style.borderBottom = "4px groove var(--golden-yellow)";
+    closeBtn.style.borderRight = "4px ridge var(--golden-yellow)";
+    closeBtn.style.borderLeft = "4px groove var(--golden-yellow)";
+    closeBtn.style.background = "radial-gradient(circle, #8b0000, #8b0000)";
+    closeBtn.style.fontSize = "25px";
+    closeBtn.style.margin = "5px";
+    closeBtn.style.color = "var(--golden-yellow)";
+    closeBtn.style.borderRadius = "8px";
+    closeBtn.addEventListener("click", ()=>{
+        card.remove();
+        warriorBtn.classList.remove("active");
+    })
+
+    // Append elements
+    card.appendChild(closeBtn)
+
+    document.body.appendChild(card);
 });
 
