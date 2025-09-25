051bb101b3512721357e2dfbd69d413ec06e6198
Dragoş <edragos33@gmail.com>
2025-09-25 17:31:20 +0200

    added rest button

--- script.js
+++ script.js
@@ -56,6 +56,35 @@ function createCard(button, title) {
     let strenghtValue = 10;
     let intellect = document.createElement("p");
     let intellectValue = 10;
+    let restBtn = document.createElement("Button");
+    restBtn.innerText = "Rest";
+    
+    restBtn.addEventListener("click", ()=> {
+        staminaValue += 15;
+        if(warriorBtn){
+            if(staminaValue >= 150){
+                staminaValue = 150
+            }
+        }
+        if(mageBtn){
+            if(staminaValue >= 80){
+                staminaValue = 80
+            }
+        }
+        if(thiefBtn){
+            if(staminaValue >= 120){
+                staminaValue = 120
+            }
+        }
+        rest()
+    })
+
+    const rest = () =>{
+        stamina.innerText = `Stamina: ${staminaValue}`;
+    }
+
+
+    
 
     // Add items to the character
     const itemsTitle = document.createElement("h3");
@@ -224,7 +253,7 @@ function createCard(button, title) {
     normalAttackBtn.addEventListener("click", () => {
         staminaValue -= Math.floor(Math.random() * 10) + 5;
         if (staminaValue <= 0){
-            healthValue -= Math.floor(Math.random() * 10) + 5;
+            healthValue -= Math.floor(Math.random() * 5) + 1;
         }
         reduce_stamina_normal_attack()
 
@@ -315,6 +344,7 @@ function createCard(button, title) {
     statsDiv.appendChild(mana);
     statsDiv.appendChild(strenght);
     statsDiv.appendChild(intellect);
+    statsDiv.appendChild(restBtn);
     card.appendChild(itemsTitle);
     card.appendChild(itemsInput);
     card.appendChild(moneyCount);
